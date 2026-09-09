4c2bce5515d143c2c19a5628c4e2011bb4c0d8ff
Jan Skwarek <janskwarek08@gmail.com>
2026-09-09 09:11:44 +0200

    Obsługa wielu typów pliku

--- Notes/NotePage.xaml.cs
+++ Notes/NotePage.xaml.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Storage;
 
 namespace Notes;
 
 public partial class NotePage : ContentPage
 {
+    private string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
     public NotePage()
     {
         InitializeComponent();
+        if(File.Exists(_fileName))TextEditor.Text = File.ReadAllText(_fileName);
     }
 
     private void SaveButton_Clicked(object? sender, EventArgs e)
